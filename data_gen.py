0a9b624996926dfeb297adec775475428942201f
junykol22 <klmedu001@myuct.ac.za>
2014-09-02 02:18:53 +0200

    Updated training data and training data generator
    updated from vula

--- data_gen.py
+++ data_gen.py
@@ -10,8 +10,8 @@ no_training_samples_per_dim = 50
 no_inputs = 2
 no_hidden = 150
 no_out = 2
-learning_rate = 0.25
-mse_cutoff = 0.01
+learning_rate = 0.075
+mse_cutoff = 0.1
 file_name = "training_data.txt"
 
 
@@ -31,9 +31,9 @@ two normalized vectors ranges from -1 through to 1:
 dot_look_mine_vec = np.linspace(-1,1,no_training_samples_per_dim)
 dot_look_super_mine_or_rock_vec = np.linspace(-1,1,no_training_samples_per_dim)
 #if we're pointing towards the mine do not turn:
-resp_dot_look_mine_vec = np.zeros(len(dot_look_mine_vec))
-for i in range(0,len(dot_look_mine_vec)):
-    resp_dot_look_mine_vec[i] = 1 if dot_look_mine_vec[i] < 0.95 else 0  
+resp_dot_look_mine_vec = np.ones(len(dot_look_mine_vec)) #always try to turn closer to the direction of the mine
+#for i in range(0,len(dot_look_mine_vec)):
+#    resp_dot_look_mine_vec[i] = 1 if dot_look_mine_vec[i] < 0.95 else 0  
 #if we're pointing towards the super mine / rock turn sharply, 
 #if we're pointing somewhere between perpendicular and the same direction stop turning:
 resp_dot_look_super_mine_or_rock_vec = np.zeros(len(dot_look_super_mine_or_rock_vec))
@@ -49,11 +49,11 @@ f.write(str(learning_rate)+"\n")
 f.write(str(mse_cutoff)+"\n")
 for m in range(0,no_training_samples_per_dim):
     print "Dumping state space ... %f%% complete" % (m/float(no_training_samples_per_dim)*100.0)
-    for r in range(0,no_training_samples_per_dim):
-        for sm in range(0,no_training_samples_per_dim):
-                f.write(str(dot_look_mine_vec[m]) + " " + 
-                        str(dot_look_super_mine_or_rock_vec[sm]) + "\n")
-                f.write(str(resp_dot_look_mine_vec[m] if resp_dot_look_super_mine_or_rock_vec[sm] == 0 else 0) + " " + 
-                        str(resp_dot_look_super_mine_or_rock_vec[sm]) + "\n")
+    #for r in range(0,no_training_samples_per_dim):
+    for sm in range(0,no_training_samples_per_dim):
+        f.write(str(dot_look_mine_vec[m]) + " " + 
+                str(dot_look_super_mine_or_rock_vec[sm]) + "\n")
+        f.write(str(resp_dot_look_mine_vec[m] if resp_dot_look_super_mine_or_rock_vec[sm] == 0 else 0) + " " + 
+                str(resp_dot_look_super_mine_or_rock_vec[sm]) + "\n")
 print "Complete... Terminating"                  
 f.close() 
